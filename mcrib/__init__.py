54d9eeac631c3baf989bd94260f8dd0c9eb2c299
Mark Mollineaux <bufordsharkley@gmail.com>
2014-05-10 01:21:10 -0700

    A location API is commited

--- mcrib/__init__.py
+++ mcrib/__init__.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+import csv
+
+from flask import Flask, render_template, jsonify
 
 from twitterapi import twitter_blueprint
 
@@ -11,6 +13,20 @@ app.register_blueprint(twitter_blueprint)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/locations/')
+def mcdonalds_locations():
+    with open('mcrib/static/mcdonaldslocations.csv') as f:
+        f.readline()  # burn the header line...
+        alllatlong = []
+        for line in f:
+            splitline = line.split(',')
+            address = splitline[2]
+            lat = splitline[5]
+            lon = splitline[6]
+            latlon = {'address': address, 'latitude': lat, 'longitude': lon}
+            alllatlong.append(latlon)
+    return jsonify({'locations': alllatlong})
 
 
 if __name__ == '__main__':
